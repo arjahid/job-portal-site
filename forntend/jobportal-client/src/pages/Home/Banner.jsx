99c3abe12a4cf65290baa30d09bf9c4df68184e8
arjahid <jhjahidhasanar@gmail.com>
2025-03-16 22:38:08 +0600

    added

--- forntend/jobportal-client/src/pages/Home/Banner.jsx
+++ forntend/jobportal-client/src/pages/Home/Banner.jsx
@@ -1,39 +1,66 @@
-import React from 'react';
-import { motion } from "motion/react"
+import React from "react";
+import { motion } from "motion/react";
+import team1 from "../../assets/img/businesspeople-having-informal-meeting.jpg";
+import team2 from "../../assets/img/young-startup-team-celebrating-business-success-while-using-computer-office.jpg";
 const Banner = () => {
-    return (
-        <div className="hero bg-base-200 min-h-96">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-   <div className='flex-1'>
-   <img
-      src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-      className="max-w-sm rounded-lg shadow-2xl" />
-   </div>
-    <div className='flex-1'>
-    <motion.h1
-            animate={{ x: [0, 50, 0], color: ['#ff0000', '#0000ff', '#00ff00'] }}
-            transition={{ duration: 2, delay: 1, ease: 'easeInOut', repeat: Infinity }}
+  return (
+    <div className="hero bg-base-200 min-h-96">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="flex-1">
+          <motion.img
+            animate={{ y: [50, 100, 50] }}
+            transition={{ duration: 10, repeat: Infinity }}
+            src={team1}
+            className="max-w-sm rounded-t-[40px] rounded-br-[40px] shadow-2xl border-l-4 border-b-4 border-blue-500"
+          />
+          <motion.img
+            animate={{ x: [100, 150, 100] }}
+            transition={{ duration: 10, repeat: Infinity }}
+            src={team2}
+            className="max-w-sm rounded-t-[40px] rounded-br-[40px] shadow-2xl border-l-4 border-b-4 border-blue-500"
+          />
+        </div>
+        <div className="flex-1">
+          <motion.h1
+            animate={{
+              x: [0, 50, 0],
+              color: ["#ff0000", "#0000ff", "#00ff00"],
+            }}
+            transition={{
+              duration: 2,
+              delay: 1,
+              ease: "easeInOut",
+              repeat: Infinity,
+            }}
             className="text-5xl font-bold"
           >
-            Job{' '}
+            Job{" "}
             <motion.span
-              animate={{ x: [0, 50, 0], color: ['#ff0000', '#0000ff', '#00ff00'] }}
-              transition={{ duration: 2, delay: 1, ease: 'easeInOut', repeat: Infinity }}
-            
+              animate={{
+                x: [0, 50, 0],
+                color: ["#ff0000", "#0000ff", "#00ff00"],
+              }}
+              transition={{
+                duration: 2,
+                delay: 1,
+                ease: "easeInOut",
+                repeat: Infinity,
+              }}
             >
               Latest
-            </motion.span>{' '}
+            </motion.span>{" "}
             News!
           </motion.h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-      <button className="btn btn-primary">Get Started</button>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default Banner;
