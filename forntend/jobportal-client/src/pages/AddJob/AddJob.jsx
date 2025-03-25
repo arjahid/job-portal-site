9220c6e538d92b2286ea637e01923a11e04a6bf7
arjahid <jhjahidhasanar@gmail.com>
2025-03-25 15:01:44 +0600

    added

--- forntend/jobportal-client/src/pages/AddJob/AddJob.jsx
+++ forntend/jobportal-client/src/pages/AddJob/AddJob.jsx
@@ -1,50 +1,165 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Swal from 'sweetalert2';
+import AuthContext from '../../context/AuthContex/AuthContex';
 
 const AddJob = () => {
+    const { user } = useContext(AuthContext);
+
+    const handleAddJob = e => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const initialData = Object.fromEntries(formData.entries());
+        const { min, max, currency, ...newJob } = initialData;
+        newJob.salaryRange = { min, max, currency };
+        newJob.requirements = newJob.requirements.split('\n');
+        newJob.responsibilities = newJob.responsibilities.split('\n');
+        console.log(newJob);
+
+        fetch('https://job-portal-server-for-recruiter-part3.vercel.app/jobs', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newJob)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Job Has been added.",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                    navigate('/myPostedJobs');
+                }
+            });
+    };
+
     return (
         <div className="container mx-auto p-4">
-            <h2 className="text-3xl font-bold mb-4 text-center">Add a New Job</h2>
-            <form action="#" method="POST" className="bg-green-700 shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
-                <h2 className="text-2xl font-semibold mb-4">Project Manager Job Application</h2>
-
+            <h2 className="text-3xl font-bold mb-4 text-center">Post a New Job</h2>
+            <form onSubmit={handleAddJob} className="bg-blue-700 shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
                 {/* Job Title */}
-                <label htmlFor="title" className="block text-blue-700 text-sm font-bold mb-2">Job Title:</label>
-                <input type="text" id="title" name="title" className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline mb-4" placeholder="Enter job title" />
-
-                {/* Company Name */}
-                <label htmlFor="company" className="block text-blue-700 font-bold text-sm font-bold mb-2">Company Name:</label>
-                <input type="text" id="company" name="company" className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline mb-4" placeholder="Enter company name" />
-
-                {/* Company Logo */}
-                <label htmlFor="company_logo" className="block text-blue-700 font-bold text-sm font-bold mb-2">Company Logo URL:</label>
-                <input type="url" id="company_logo" name="company_logo" className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline mb-4" placeholder="Enter company logo URL" />
-
-                {/* Location */}
-                <label htmlFor="location" className="block text-blue-700 font-bold text-sm font-bold mb-2">Location:</label>
-                <input type="text" id="location" name="location" className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline mb-4" placeholder="Enter location" />
-
+                <div className="form-control mb-4">
+                    <label className="label">
+                        <span className="label-text">Job Title</span>
+                    </label>
+                    <input type="text" name="title" placeholder="Job Title" className="input input-bordered w-full" required />
+                </div>
+                {/* Job Location */}
+                <div className="form-control mb-4">
+                    <label className="label">
+                        <span className="label-text">Job Location</span>
+                    </label>
+                    <input type="text" name="location" placeholder="Job Location" className="input input-bordered w-full" required />
+                </div>
                 {/* Job Type */}
-                <label htmlFor="jobType" className="block text-gray-700 text-sm font-bold mb-2">Job Type:</label>
-                <input type="text" id="jobType" name="jobType" className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline mb-4" placeholder="Enter job type" />
-
-                {/* Description */}
-                <label htmlFor="description" className="block text-blue-700 font-bold text-sm font-bold mb-2">Job Description:</label>
-                <textarea id="description" name="description" rows="4" className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline mb-4" placeholder="Enter job description"></textarea>
-
-                {/* Requirements */}
-                <label htmlFor="requirements" className="block text-blue-700 font-bold text-sm font-bold mb-2">Requirements:</label>
-                <textarea id="requirements" name="requirements" rows="4" className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline mb-4" placeholder="Enter requirements"></textarea>
-
+                <div className="form-control mb-4">
+                    <label className="label">
+                        <span className="label-text">Job Type</span>
+                    </label>
+                    <select name="jobType" className="select select-bordered w-full" required>
+                        <option disabled selected>Pick a Job type</option>
+                        <option>Full-time</option>
+                        <option>Intern</option>
+                        <option>Part-time</option>
+                    </select>
+                </div>
+                {/* Job Field */}
+                <div className="form-control mb-4">
+                    <label className="label">
+                        <span className="label-text">Job Field</span>
+                    </label>
+                    <select name="jobField" className="select select-bordered w-full" required>
+                        <option disabled selected>Pick a Job Field</option>
+                        <option>Engineering</option>
+                        <option>Marketing</option>
+                        <option>Finance</option>
+                        <option>Teaching</option>
+                    </select>
+                </div>
                 {/* Salary Range */}
-                <label htmlFor="salaryRange" className="block text-blue-700 font-bold text-sm font-bold mb-2">Salary Range:</label>
-                <input type="text" id="salaryRange" name="salaryRange" className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline mb-4" placeholder="Enter salary range" />
-
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mb-4">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Salary Range</span>
+                        </label>
+                        <input type="text" name="min" placeholder="Min" className="input input-bordered w-full" required />
+                    </div>
+                    <div className="form-control">
+                        <input type="text" name="max" placeholder="Max" className="input input-bordered w-full" required />
+                    </div>
+                    <div className="form-control">
+                        <select name="currency" className="select select-bordered w-full" required>
+                            <option disabled selected>Currency</option>
+                            <option>BDT</option>
+                            <option>USD</option>
+                            <option>INR</option>
+                        </select>
+                    </div>
+                </div>
+                {/* Job Description */}
+                <div className="form-control mb-4">
+                    <label className="label">
+                        <span className="label-text">Job Description</span>
+                    </label>
+                    <textarea name="description" placeholder="Job Description" className="textarea textarea-bordered w-full" required></textarea>
+                </div>
+                {/* Company Name */}
+                <div className="form-control mb-4">
+                    <label className="label">
+                        <span className="label-text">Company Name</span>
+                    </label>
+                    <input type="text" name="company" placeholder="Company Name" className="input input-bordered w-full" required />
+                </div>
+                {/* Job Requirements */}
+                <div className="form-control mb-4">
+                    <label className="label">
+                        <span className="label-text">Job Requirements</span>
+                    </label>
+                    <textarea name="requirements" placeholder="Put each requirement on a new line" className="textarea textarea-bordered w-full" required></textarea>
+                </div>
+                {/* Job Responsibilities */}
+                <div className="form-control mb-4">
+                    <label className="label">
+                        <span className="label-text">Job Responsibilities</span>
+                    </label>
+                    <textarea name="responsibilities" placeholder="Write each responsibility on a new line" className="textarea textarea-bordered w-full" required></textarea>
+                </div>
+                {/* HR Name */}
+                <div className="form-control mb-4">
+                    <label className="label">
+                        <span className="label-text">HR Name</span>
+                    </label>
+                    <input type="text" name="hr_name" placeholder="HR Name" className="input input-bordered w-full" required />
+                </div>
+                {/* HR Email */}
+                <div className="form-control mb-4">
+                    <label className="label">
+                        <span className="label-text">HR Email</span>
+                    </label>
+                    <input type="email" name="hr_email" defaultValue={user?.email} placeholder="HR Email" className="input input-bordered w-full" required />
+                </div>
                 {/* Application Deadline */}
-                <label htmlFor="applicationDeadline" className="block text-blue-700 font-bold text-sm font-bold mb-2">Application Deadline:</label>
-                <input type="date" id="applicationDeadline" name="applicationDeadline" className="shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline mb-4" />
-
+                <div className="form-control mb-4">
+                    <label className="label">
+                        <span className="label-text">Application Deadline</span>
+                    </label>
+                    <input type="date" name="applicationDeadline" className="input input-bordered w-full" required />
+                </div>
+                {/* Company Logo URL */}
+                <div className="form-control mb-4">
+                    <label className="label">
+                        <span className="label-text">Company Logo URL</span>
+                    </label>
+                    <input type="url" name="company_logo" placeholder="Company Logo URL" className="input input-bordered w-full" required />
+                </div>
                 {/* Submit Button */}
-                <input type="submit" value="Add Job" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" />
+                <div className="form-control mt-6">
+                    <button type="submit" className="btn btn-primary w-full">Submit</button>
+                </div>
             </form>
         </div>
     );
