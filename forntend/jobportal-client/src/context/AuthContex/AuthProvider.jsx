51b45d037b16e98678e071255589d5625e752632
arjahid <jhjahidhasanar@gmail.com>
2025-04-11 15:21:01 +0600

    crated token

--- forntend/jobportal-client/src/context/AuthContex/AuthProvider.jsx
+++ forntend/jobportal-client/src/context/AuthContex/AuthProvider.jsx
@@ -1,59 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import AuthContext from './AuthContex';
-import {  createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
-import { auth } from '../../firebase/firebase.init';
+import React, { useEffect, useState } from "react";
+import AuthContext from "./AuthContex";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../../firebase/firebase.init";
+import axios from "axios";
 
-const googleProvider=new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
 
-const AuthProvider = ({children}) => {
-    const [user,setUser]=useState(null);
-    const [loading,setLOading]=useState(true);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLOading] = useState(true);
 
-    const createUser=(email,password)=>{
-        setLOading(true);
-        return createUserWithEmailAndPassword(auth,email,password)
-   
-    }
-    
-    const signInUser=(email,password)=>{
-        setLOading(true);
-        return signInWithEmailAndPassword(auth,email,password)
-    }
-    const signInGoogle=()=>{
-        setLOading(true);
-        return signInWithPopup(auth,googleProvider)
-    }
-    const signOutUser=()=>{
-        setLOading(true);
-        return signOut(auth);
-    }
+  const createUser = (email, password) => {
+    setLOading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
+  const signInUser = (email, password) => {
+    setLOading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const signInGoogle = () => {
+    setLOading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+  const signOutUser = () => {
+    setLOading(true);
+    return signOut(auth);
+  };
 
-    const authInfo={
-        user,
-        loading,
-        createUser,
-        signInUser,signOutUser,
-        signInGoogle
-        
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signInUser,
+    signOutUser,
+    signInGoogle,
+  };
 
-    }
-
-    useEffect(()=>{
-        const unsubscribe=onAuthStateChanged(auth,currentUser=>{
-            setUser(currentUser);
-            console.log('state captured',currentUser);
-            setLOading(false);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log("state captured", currentUser);
+      if (currentUser?.email) {
+        const user = { email: currentUser.email };
+        axios.post("http://localhost:5173/jwt", user, {
+          withCredentials: true,
         })
-        return ()=>{
-            unsubscribe();
-        }
-    },[])
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+        .then(res=>console.log(res.data));
+      }
+      setLOading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
