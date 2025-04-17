5cb30f46c734e2bb3b3f922880dbd5c14d401b2c
arjahid <jhjahidhasanar@gmail.com>
2025-04-18 02:05:52 +0600

    fix responsive

--- forntend/jobportal-client/src/pages/Home/HotJobs.jsx
+++ forntend/jobportal-client/src/pages/Home/HotJobs.jsx
@@ -12,14 +12,14 @@ const HotJobs = () => {
             });
     }, []);
     return (
-        <div className="p-4 sm:p-6 lg:p-8 bg-gray-100">
+        <div className="p-4 sm:p-6 lg:p-8 bg-blue-400">
             <div className="text-center mb-8">
-                <h2 className="text-3xl font-bold text-gray-800">Hot Jobs</h2>
-                <p className="text-gray-600 mt-2">
+                <h2 className="text-2xl sm:text-3xl font-bold text-gray-800">Hot Jobs</h2>
+                <p className="text-sm sm:text-base text-gray-600 mt-2">
                     Explore the latest job opportunities tailored for you.
                 </p>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6">
                 {jobs.map(job => (
                     <HotJobsCard job={job} key={job._id}></HotJobsCard>
                 ))}
