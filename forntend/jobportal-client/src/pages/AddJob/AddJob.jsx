62c7f8f734d7b95fd766b1ed883276b8d4eb3d99
arjahid <jhjahidhasanar@gmail.com>
2025-04-18 00:49:20 +0600

    change url for production

--- forntend/jobportal-client/src/pages/AddJob/AddJob.jsx
+++ forntend/jobportal-client/src/pages/AddJob/AddJob.jsx
@@ -17,7 +17,7 @@ const AddJob = () => {
         newJob.responsibilities = newJob.responsibilities.split('\n');
         console.log(newJob);
 
-        fetch('http://localhost:3000/jobs', {
+        fetch('https://job-portal-backend.onrender.com/jobs', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
